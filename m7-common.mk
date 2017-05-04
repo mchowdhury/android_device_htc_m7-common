cde39f52a1e8325408c30eb3fbda26c39bc16a6f
Pawin Vongmasa <pawin@google.com>
2017-05-03 19:54:12 -0700

    media: Create media_profiles_V1_0.xml
    media_profiles_V1_0.xml is created from media_profiles.xml by replacing
    the dtd schema inside media_profiles.xml with an external reference to
    /system/etc/media_profiles_V1_0.dtd.
    
    Test: Compiles
    
    Bug: 36430270
    
    From: Pawin Vongmasa <pawin@google.com>
    Date: Wed May 03 22:42:57 2017 -0700
    
        Remove the property media.settings.xml.
    
        This CL also removes media_profiles.xml as it should be replaced by
        media_profiles_V1_0.xml.
    
        Bug: 36430270
        Merged-In: I4299abfde44ced36560736520d0be592894d00a7
        Change-Id: I4299abfde44ced36560736520d0be592894d00a7
    
    Merged-In: I2bdd38bc8e85cdb64c4729a6ebc843f5031c127c
    Change-Id: I2bdd38bc8e85cdb64c4729a6ebc843f5031c127c
    Signed-off-by: Chippa-a <vusal1372@gmail.com>
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- m7-common.mk
+++ m7-common.mk
@@ -1,4 +1,5 @@
 # Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -83,7 +84,7 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+    $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:system/etc/media_profiles_V1_0.xml
 
 # NFC
 PRODUCT_PACKAGES += \
